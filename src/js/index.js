c54ad80284d9a461268317245dad62df71bb6eb5
AlexanderKokhno <71799901+AlexanderKokhno@users.noreply.github.com>
2021-05-10 09:57:07 -0400

    push

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,17 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-// const key = "YOURKEYHERE";
+const key = "https://mugenmonkey.com/api/v0/ds3_builds";
 
 const query = async function () {
     try {
-        const response = await fetch("https://mugenmonkey.com/api/v0/ds3_weapons"
+        const response = await fetch("https://mugenmonkey.com/api/v0/ds3_builds"
         );
         const data = await response.json();
-        console.log(data);
+        console.log(data.results);
     }   catch (error) {
-        console.log(error)
-        alert("Yep, something went wrong! (╬ಠ益ಠ)")
+        console.log(error);
+        alert("Yep, something went wrong! (╬ಠ益ಠ)");
     }
 };
 query();
