dbe93b523bded7d235248175374ab01496005466
AlexanderKokhno <71799901+AlexanderKokhno@users.noreply.github.com>
2021-05-09 22:43:33 -0400

    good enough and good night

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,17 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-const key = "YOURKEYHERE";
+// const key = "YOURKEYHERE";
+
+const query = async function () {
+    try {
+        const response = await fetch("https://mugenmonkey.com/api/v0/ds3_weapons"
+        );
+        const data = await response.json();
+        console.log(data);
+    }   catch (error) {
+        console.log(error)
+        alert("Yep, something went wrong! (╬ಠ益ಠ)")
+    }
+};
+query();
