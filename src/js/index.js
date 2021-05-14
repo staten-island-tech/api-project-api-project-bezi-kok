47dcd098f400eebbfabeca2c87c9c5607fdd19e4
AlexanderKokhno <71799901+AlexanderKokhno@users.noreply.github.com>
2021-05-13 22:47:14 -0400

    3rw4te5rsy75erdsyfgt43wetrd

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,15 @@ import { genres } from "./genre";
 
 const query = async function () {
     try {
-        const response = await fetch("https://mugenmonkey.com/api/v0/ds3_weapons"
+        const response = await fetch("https://mugenmonkey.com/api/v0/ds3_builds?per_page=100&page=100"
         );
         const data = await response.json();
-        console.log(data);
+        data.results.forEach(build => {
+            DOMSelectors.grid.insertAdjacentHTML(
+                "beforeend",
+                ``
+                );
+        });
     }   catch (error) {
         console.log(error)
         alert("Yep, something went wrong! (╬ಠ益ಠ)")
