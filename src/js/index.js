304310cc0296a10f1acdfe10bc3c194af6c6138e
AlexanderKokhno <71799901+AlexanderKokhno@users.noreply.github.com>
2021-05-26 19:59:06 -0400

    e2qwfgdhfnhrewq

--- src/js/index.js
+++ src/js/index.js
@@ -24,58 +24,58 @@ const query = async function () {
                 </div>
                 <div class="core-stats">
                   <p class="build-level">${DS3build.level}</p>
-                  <p class="build-vigor">hyt5</p>
-                  <p class="build-attunement">htr5</p>
-                  <p class="build-endurance">jdj</p>
-                  <p class="build-vitality">ebfc</p>
-                  <p class="build-strength">ufdnu</p>
-                  <p class="build-dexterity">fneuws</p>
-                  <p class="build-intelligence">fefs</p>
-                  <p class="build-faith">fsds</p>
-                  <p class="build-luck">fdss</p>
-                  <p class="build-hollowing">dfd</p>
-                  <p class="build-covenant">wknfsduwnusafdnuwe</p>
-                  <p class="build-two-hand">wedwq</p>
+                  <p class="build-vigor">${DS3build.vigor}</p>
+                  <p class="build-attunement">${DS3build.attunement}</p>
+                  <p class="build-endurance">${DS3build.endurance}</p>
+                  <p class="build-vitality">${DS3build.vitality}</p>
+                  <p class="build-strength">${DS3build.strength}</p>
+                  <p class="build-dexterity">${DS3build.dexterity}</p>
+                  <p class="build-intelligence">${DS3build.intelligence}</p>
+                  <p class="build-faith">${DS3build.faith}</p>
+                  <p class="build-luck">${DS3build.luck}</p>
+                  <p class="build-hollowing">${DS3build.hollowing}</p>
+                  <p class="build-covenant">${DS3build.covenant}</p>
+                  <p class="build-two-hand">${DS3build.two_hand}</p>
                 </div>
-                <div class="build-add-ons">
-                  <p class="build-head-id">dwd</p>
-                  <p class="build-body-id">wdws</p>
-                  <p class="build-arms-id">wdds</p>
-                  <p class="build-legs-id">wdds</p>
-                  <p class="build-ring1-id">wdds</p>
-                  <p class="build-ring2-id">wdds</p>
-                  <p class="build-ring3-id">wdds</p>
-                  <p class="build-ring4-id">wdds</p>
-                  <p class="build-1h1-id">wdds</p>
-                  <p class="build-1h2-id">wdds</p>
-                  <p class="build-1h3-id">wdds</p>
-                  <p class="build-rh1-id">ewfg</p>
-                  <p class="build-rh2-id">ewfg</p>
-                  <p class="build-rh3-id">ewfg</p>
-                </div>
-                <div class="build-spells">
-                  <p class="build-spell1-id">jusw</p>
-                  <p class="build-spell2-id">jusw</p>
-                  <p class="build-spell3-id">jusw</p>
-                  <p class="build-spell4-id">jusw</p>
-                  <p class="build-spell5-id">jusw</p>
-                  <p class="build-spell6-id">jusw</p>
-                  <p class="build-spell7-id">jusw</p>
-                  <p class="build-spell8-id">jusw</p>
-                  <p class="build-spell9-id">jusw</p>
-                  <p class="build-spell10-id">jusw</p>
-                </div>
+                // <div class="build-add-ons">
+                //   <p class="build-head-id">dwd</p>
+                //   <p class="build-body-id">wdws</p>
+                //   <p class="build-arms-id">wdds</p>
+                //   <p class="build-legs-id">wdds</p>
+                //   <p class="build-ring1-id">wdds</p>
+                //   <p class="build-ring2-id">wdds</p>
+                //   <p class="build-ring3-id">wdds</p>
+                //   <p class="build-ring4-id">wdds</p>
+                //   <p class="build-1h1-id">wdds</p>
+                //   <p class="build-1h2-id">wdds</p>
+                //   <p class="build-1h3-id">wdds</p>
+                //   <p class="build-rh1-id">ewfg</p>
+                //   <p class="build-rh2-id">ewfg</p>
+                //   <p class="build-rh3-id">ewfg</p>
+                // </div>
+                // <div class="build-spells">
+                //   <p class="build-spell1-id">jusw</p>
+                //   <p class="build-spell2-id">jusw</p>
+                //   <p class="build-spell3-id">jusw</p>
+                //   <p class="build-spell4-id">jusw</p>
+                //   <p class="build-spell5-id">jusw</p>
+                //   <p class="build-spell6-id">jusw</p>
+                //   <p class="build-spell7-id">jusw</p>
+                //   <p class="build-spell8-id">jusw</p>
+                //   <p class="build-spell9-id">jusw</p>
+                //   <p class="build-spell10-id">jusw</p>
+                // </div>
                 <div class="author-details">
-                  <p class="author-name">2wdwasrdfweasf</p>
-                  <p class="creation-date">2re23wrd32wrdc3ews</p>
-                  <p class="updated-date">ewsdfcewsdzfcedw</p>
+                  <p class="author-name">${DS3build.author}</p>
+                  <p class="creation-date">${DS3build.created_at}</p>
+                  <p class="updated-date">${DS3build.updated_at}</p>
                 </div>
               </div>`
             );
         });
     }   catch (error) {
         console.log(error);
-        alert("Yep, something went wrong! (╬ಠ益ಠ)");
+        alert("Yep, something went wrong, for the millionth time! (╬ಠ益ಠ)");
         }
 };
 query();
