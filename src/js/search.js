59edbae006d630fd1f40074e32d0b9c7c4cabf90
mr. whalen <michael.whalen@sitechhs.com>
2021-05-27 09:33:04 -0400

    iASYHDBIFPLKA

--- src/js/search.js
+++ src/js/search.js
@@ -2,12 +2,13 @@ import { DOMSelectors } from "./DOM";
 // import {genres} from "genre";
 
 const listen = function () {
-    DOMSelectors.searchForm.addEventListener("submit", function(e) {
-        e.preventDefault();
-        const searchParams = DOMSelectors.searchArea.nodeValue;
-        // const searchQuery = //input chunk of index.js here
-    });
-}
+  DOMSelectors.searchForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const searchParams = DOMSelectors.searchArea.value;
+
+    // const searchQuery = //input chunk of index.js here
+  });
+};
 
 // searchQuery();
-// listen();
+listen();
