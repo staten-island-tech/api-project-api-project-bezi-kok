9c26063efadcc8be4f5a9389cf56c05f3564e67c
AlexanderKokhno <71799901+AlexanderKokhno@users.noreply.github.com>
2021-05-27 19:39:37 -0400

    wqrdqwrfdcwefdsfe

--- src/js/index.js
+++ src/js/index.js
@@ -6,15 +6,15 @@ const key = "https://mugenmonkey.com/api/v0/ds3_builds";
 const query = async function () {
   try {
     const response = await fetch(
-      `https://mugenmonkey.com/api/v0/ds3_builds?per_page=5&page=5`
+      `https://mugenmonkey.com/api/v0/ds3_builds?`
     );
     const data = await response.json();
-    const DS3build = data.ds3_builds[460458];
+    const DS3build = data.ds3_builds[460551];
     // parsedData.ds3_builds.forEach((DS3build) => {
 
     DOMSelectors.grid.insertAdjacentHTML(
       "beforeend",
-      `<div class="info-div-build-stats">
+      `<div class="info-api-div-build-stats">
                 <div class="main-info">
                 <p class="build-title>Title: ${DS3build.title}</p>
                   <p class="build-gender">Gender: ${DS3build.gender}</p>
@@ -38,34 +38,6 @@ const query = async function () {
                   <p class="build-covenant">Covenant: ${DS3build.covenant}</p>
                   <p class="build-two-hand">Two_Hand: ${DS3build.two_hand}</p>
                 </div>
-                // <div class="build-add-ons">
-                //   <p class="build-head-id">dwd</p>
-                //   <p class="build-body-id">wdws</p>
-                //   <p class="build-arms-id">wdds</p>
-                //   <p class="build-legs-id">wdds</p>
-                //   <p class="build-ring1-id">wdds</p>
-                //   <p class="build-ring2-id">wdds</p>
-                //   <p class="build-ring3-id">wdds</p>
-                //   <p class="build-ring4-id">wdds</p>
-                //   <p class="build-1h1-id">wdds</p>
-                //   <p class="build-1h2-id">wdds</p>
-                //   <p class="build-1h3-id">wdds</p>
-                //   <p class="build-rh1-id">ewfg</p>
-                //   <p class="build-rh2-id">ewfg</p>
-                //   <p class="build-rh3-id">ewfg</p>
-                // </div>
-                // <div class="build-spells">
-                //   <p class="build-spell1-id">jusw</p>
-                //   <p class="build-spell2-id">jusw</p>
-                //   <p class="build-spell3-id">jusw</p>
-                //   <p class="build-spell4-id">jusw</p>
-                //   <p class="build-spell5-id">jusw</p>
-                //   <p class="build-spell6-id">jusw</p>
-                //   <p class="build-spell7-id">jusw</p>
-                //   <p class="build-spell8-id">jusw</p>
-                //   <p class="build-spell9-id">jusw</p>
-                //   <p class="build-spell10-id">jusw</p>
-                // </div>
                 <div class="author-details">
                   <p class="author-name">Author's name: ${DS3build.author}</p>
                   <p class="creation-date">Created On: ${DS3build.created_at}</p>
@@ -78,6 +50,8 @@ const query = async function () {
     alert("Yep, something went wrong, for the millionth time! (╬ಠ益ಠ)");
   }
 };
+
+
 query();
 // searchQuery();
 // listen();
